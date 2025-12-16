b10e7ddb642f483b537db05d489cc33a65d5fa05
Nicolas <nicolasbruno874@gmail.com>
2025-12-16 19:04:26 -0300

    Initialize project with Spring Boot backend and React frontend
    Add initial backend using Spring Boot with Maven, including basic application and test setup.
    
    Add frontend using React and Vite, including configuration files, initial assets, and ESLint setup.

---
+++ Backend/src/main/java/com/redline/redline_backend/RedlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redline.redline_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedlineBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/redline/redline_backend/RedlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redline.redline_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
